76ee1f639396659ce3ddf689d75dca01d0231c21
unknown <tirritoroberto@gmail.com>
2021-12-06 17:39:10 +0100

    funziona

--- 06-12-21/esercizio/index.js
+++ 06-12-21/esercizio/index.js
@@ -1,13 +1,31 @@
 const fs = require('fs').promises;
 const arguments = process.argv.slice(2)
 
-console.log(arguments)
+// console.log(arguments)
 
+
+//leggere da console il file di testo indicato 
 async function readFile(file){
     const data=await fs.readFile(file)
     console.log(data.toString())
 }
+console.log(readFile('./lista.txt'))
 
-console.log(readFile('./readme.md'))
+
+//scrive su file con sintassi html
+async function writeFile(file, content){
+    await fs.writeFile(file, content)
+}
+const text=
+`
+<ul>
+<li>ciao</li>
+<li>cia</li>
+<li>ci</li>
+</ul>
+
+`
+writeFile('lista.txt', text)
+
 // fs.readFile()
 // fs.writeFile(file, content, { flag: 'a+' } )
