03075fcef56ffa02e4745bc0cd8dc1ced8e0dd36
unknown <tirritoroberto@gmail.com>
2021-12-06 17:19:01 +0100

    stampa readme come prova ok

--- 06-12-21/esercizio/index.js
+++ 06-12-21/esercizio/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs').promises;
 const arguments = process.argv.slice(2)
-console.log(arguments, "ciao");
 
-fs.readFile()
-fs.writeFile(file, content, { flag: 'a+' } )
+console.log(arguments)
+
+async function readFile(file){
+    const data=await fs.readFile(file)
+    console.log(data.toString())
+}
+
+console.log(readFile('./readme.md'))
+// fs.readFile()
+// fs.writeFile(file, content, { flag: 'a+' } )
